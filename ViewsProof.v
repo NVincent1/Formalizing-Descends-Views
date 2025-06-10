334d07702792fe95b1f7cdfc9610d2c3b2d4a9cb
Nicolas VINCENT <nicolas.vincent1@ens-rennes.fr>
2025-06-10 15:19:01 +0200

    stronger condition than injectivity

--- ViewsProof.v
+++ ViewsProof.v
@@ -13,152 +13,174 @@ Axiom FunEquality :
   f = g <-> forall (x : T1), f x = g x
 .
 
+Theorem TrueEquality :
+  forall (T : Type) (x : T) (y : T),
+  (fun _ : True => x) = (fun _ : True => y) -> x = y.
+Proof.
+  intros. apply FunEquality in H. apply H. apply I.
+Qed.
+
+Axiom TrueFun :
+  forall (T : Type) (f : True -> T) (g : True -> T) (x : True),
+  f x = g x -> f = g.
+
 Notation "h :: tl" := (Cons _ h tl).
 Notation "[]" := (Nil _).
+
 
 Fixpoint Tuple (d : List nat) : Type :=
   match d with
   | [] => True
-  | n::[] => Idx n
   | n::tl => Idx n * (Tuple tl)
 end.
 
 Definition ViewArray (d : List nat) : Type :=
-  match d with
-  | [] => nat
-  | d =>  Tuple d -> nat
-end.
+  Tuple d -> nat
+.
 
-Notation "[[ T ; n ]]" := (ViewArray (n::T)).
-Notation "[[; n ]]" := (ViewArray (n::(Nil nat))).
-Notation "[[ [[ T ; m ]] ; n ]]" := (ViewArray (n::m::T)).
-Notation "[[ [[ ; m ]] ; n ]]" := (ViewArray (n::m::[])).
+Notation "[[ T ; n ]]" := (n::T).
+Notation "[[ ; n ]]" := [[ [] ; n]].
 
-Definition to_view (n : nat) : [[; n]] :=
-  fun i => to_nat i.
+Definition to_view (n : nat) : ViewArray [[ ; n]] :=
+  fun i => match i with | (i,_) => to_nat i end.
 
 
-Definition reverse {T : List nat} {n : nat}  : [[T;n]] -> [[T;n]] :=
-  match T with
-  | [] => fun (v : [[;n]]) => fun (i : Tuple (n::[])) =>
-      v (to_Idx n (n - 1 - (to_nat i)) reverseProof)
-  | T => fun (v : [[T;n]]) => fun (i : Tuple (n::T)) =>
+Definition reverse {T : List nat} {n : nat}  (v : ViewArray [[T;n]]) : ViewArray [[T;n]] :=
+  fun (i : Tuple (n::T)) =>
       match i with | (i,j) =>
       v (to_Idx n (n - 1 - (to_nat i)) reverseProof, j)
-end
-end.
+      end
+.
 
-Definition select  {T :List nat} {l : nat} (a : nat) (b : nat) : [[T;b + l]] -> [[T;b - a]] :=
-  match T with
-  | [] => fun (v : [[;b + l]]) => fun (i : Tuple ((b-a)::[])) =>
-      v (to_Idx (b+l) (a + to_nat i) selectProof)
-  | T => fun (v : [[T;b + l]]) => fun (i : Tuple ((b-a)::T)) =>
+Definition select  {T :List nat} {l : nat} (a : nat) (b : nat) (v : ViewArray [[T;b + l]]) : ViewArray[[T;b - a]] :=
+  fun (i : Tuple ((b-a)::T)) =>
       match i with | (i,j) =>
       v (to_Idx (b+l) (a + to_nat i) selectProof, j)
-end
-end.
+      end
+.
 
-Definition group {T : List nat} (m : nat) (n : nat) : [[T ; m*n]] -> [[ [[T;m]] ; n]] :=
-  match T with
-  | [] => fun (v : [[;m*n]]) => fun (i : Tuple (n::m::[])) =>
-      match i with | (i,j) =>
-      v (to_Idx (m*n) (to_nat j + m*(to_nat i)) groupProof)
-  end
-  | T => fun (v : [[T;m*n]]) => fun (i : Tuple (n::m::T)) =>
+Definition group {T : List nat} (m : nat) (n : nat) (v : ViewArray [[T;m*n]]) : ViewArray [[ [[T;m]] ; n]] :=
+  fun (i : Tuple (n::m::T)) =>
       match i with | (i,(j,k)) =>
       v (to_Idx (m*n) (to_nat j + m*(to_nat i)) groupProof,k)
   end
-end.
+.
 
-Definition transpose {T : List nat} {m : nat} {n : nat} : [[ [[T;m]] ; n]] -> [[ [[T;n]] ; m]] :=
-  match T with
-  | [] => fun (v : [[ [[ ;m]] ; n]]) => fun (i : Tuple (m::n::[])) =>
-    match i with | (i,j) => v (j,i)
+Definition transpose {T : List nat} {m : nat} {n : nat} (v : ViewArray [[ [[T;m]] ; n]]) : ViewArray [[ [[T;n]] ; m]] :=
+  fun (i : Tuple (m::n::T)) =>
+    match i with | (i,(j,k)) =>
+    v (j,(i,k))
   end
-  | T => fun (v : [[ [[T;m]] ; n]]) => fun (i : Tuple (m::n::T)) =>
-    match i with | (i,(j,k)) => v (j,(i,k))
+.
+
+Definition partialApp {T : List nat} {n : nat} : ViewArray [[T;n]] -> Idx n -> (ViewArray T) :=
+  fun (v : ViewArray [[T;n]]) i x => v (i,x)
+.
+
+Definition map {A : List nat} {B : List nat} {n : nat} (f : (ViewArray A -> ViewArray B))  (v : ViewArray [[A;n]]) : ViewArray [[B;n]] :=
+  fun (i : Tuple (n::B)) =>
+      match i with | (i,j) =>
+      (f (partialApp v i)) j
   end
-end.
+.
 
-Definition partialApp {T : List nat} {n : nat} : [[T;n]] -> Idx n -> (ViewArray T) :=
-  match T with
-  | [] => fun (v : [[;n]]) i => (v i)
-  | T => fun (v : [[T;n]]) i x => v (i,x)
-end.
+(* The reason we use tuples is that we want total application in this property *)
+Definition isInjective {T : List nat} (f : ViewArray T) : Prop :=
+  forall (i : Tuple T) (j : Tuple T), f i = f j -> i = j
+.
 
-Definition map {A : List nat} {B : List nat} {n : nat} : (ViewArray A -> ViewArray B)  -> [[A;n]] -> [[B;n]] :=
-  match B with
-  | [] => fun f (v : [[A;n]]) (i : Tuple (n::[])) => (f (partialApp v i))
-  | B => fun f (v : [[A;n]]) (i : Tuple (n::B)) => match i with | (i,j) => (f (partialApp v i)) j
-  end
-end.
+(* A function conserve injectivity if it is injective on indexes and if an array is injective,
+then fun (i x) => f (partialApp v i) x is injective *)
+Definition conserveInjectivity {A : List nat} {B : List nat} (f : ViewArray A -> ViewArray B) : Prop :=
+  forall (n : nat) (v : ViewArray [[A;n]]),
+  isInjective v -> (forall (i : Idx n) (j : Idx n) (x : Tuple B) (y : Tuple B), f (partialApp v i) x = f (partialApp v j) y -> x = y /\ i = j)
+.
 
-
-Definition isInjective {T : List nat} : ViewArray T -> Prop :=
-  match T with 
-  | [] => fun (f : ViewArray [])  => True
-  | T => fun (f : ViewArray T) => forall (i : Tuple T) (j : Tuple T), f i = f j -> i = j
-end.
+Proposition conserveInjectiviy_conserves_injectiviy :
+  forall A B (f : ViewArray A -> ViewArray B), conserveInjectivity f -> (forall v, isInjective v -> isInjective (f v)).
+Proof.
+  unfold conserveInjectivity.
+  unfold isInjective.
+  intros.
+  assert (isInjective (fun (x : Tuple (1::A)) => match x with | (i,n) => v n end)). {
+    unfold isInjective. intros.
+    destruct i0; destruct j0. destruct i0; destruct i1. inversion H3.
+    inversion H4. subst. apply H0 in H2. rewrite H2. assert (Id 1 0 H3 = Id 1 0 H4). apply unif_Idx. rewrite H5.
+    reflexivity.
+    inversion H7. inversion H6.
+  } set (v' := fun x : Tuple [[A; 1]] => let (_, n) := x in v n) in *.
+  unfold isInjective in H2.
+  unfold partialApp in *.
+  assert (forall (a b : Idx 1) (x y : Tuple B), f (fun x0 : Tuple A => v' (a, x0)) x = f (fun x0 : Tuple A => v' (b, x0)) y -> x = y /\ a = b).
+  intros. apply H with (i := a) (j := b) (x := x) (y := y) in H2. apply H2. apply H3.
+  assert (0 < 1). apply le_n. 
+  assert (f (fun x0 => v' ((Id 1 0 H4),x0)) i = f (fun x0 => v' (Id 1 0 H4,x0)) j).
+  unfold v'. assert ((fun x0 => v x0) = v). apply FunEquality. intro. reflexivity.
+  rewrite H5. apply H1.
+  apply H3 in H5. destruct H5. apply H5.
+Qed.
 
 Proposition to_view_is_injective :
   forall (n : nat), isInjective (to_view n).
 Proof.
-  unfold isInjective. unfold to_view. intros. apply to_nat_injective in H. apply H.
+  unfold isInjective. unfold to_view. intros.
+  destruct i,j. apply to_nat_injective in H. rewrite H. destruct t,t0. reflexivity.
 Qed.
 
 Proposition reverse_conserve_injectivity :
-  forall (T:List nat) (n : nat) (v : [[T;n]]), isInjective v -> isInjective (reverse v).
+  forall T (n : nat), conserveInjectivity reverse (A := n::T) (B := n::T).
 Proof.
-    destruct T.
-    - unfold isInjective. intros. unfold reverse in H0. apply H in H0.
-      inversion H0. apply sub_injective with (c := n - 1) in H2. apply to_nat_injective in H2. apply H2.
-      + destruct n. assert (to_nat i < 0). apply BoundedInt. inversion H1. simpl. rewrite Nat.sub_0_r.
-      assert (S (to_nat i) <= S n). apply BoundedInt. apply le_S_n in H1. apply H1. 
-      + destruct n. assert (to_nat j < 0). apply BoundedInt. inversion H1. simpl. rewrite Nat.sub_0_r.
-      assert (S (to_nat j) <= S n). apply BoundedInt. apply le_S_n in H1. apply H1.
-    - unfold isInjective. intros. unfold reverse in H0. destruct i as [i xi]; destruct j as [j xj]. apply H in H0.
-      inversion H0. apply sub_injective with (c := n - 1) in H2. apply to_nat_injective in H2. rewrite H2. reflexivity.
-      + destruct n. assert (to_nat i < 0). apply BoundedInt. inversion H1. simpl. rewrite Nat.sub_0_r.
-      assert (S (to_nat i) <= S n). apply BoundedInt. apply le_S_n in H1. apply H1. 
-      + destruct n. assert (to_nat j < 0). apply BoundedInt. inversion H1. simpl. rewrite Nat.sub_0_r.
-      assert (S (to_nat j) <= S n). apply BoundedInt. apply le_S_n in H1. apply H1.
+  intros. unfold conserveInjectivity. unfold partialApp. intros.
+    unfold isInjective in H. unfold reverse in H0. destruct x; destruct y. apply H in H0. injection H0.
+    intros. apply sub_injective with (c := n - 1) in H2. apply to_nat_injective in H2. subst. split; reflexivity.
+    destruct n. destruct i0. inversion H4. simpl. rewrite Nat.sub_0_r. destruct i0. simpl. apply le_S_n. apply H4.
+    destruct n. destruct i1. inversion H4. simpl. rewrite Nat.sub_0_r. destruct i1. simpl. apply le_S_n. apply H4.
 Qed.
 
 Proposition select_conserve_injectivity :
-  forall T a b k (v : [[T;b+k]]), isInjective v -> isInjective (select a b v).
+  forall T (a : nat) (b : nat) (k : nat), conserveInjectivity (select a b) (A := [[T;b+k]]).
 Proof.
-  unfold isInjective.
+  unfold conserveInjectivity.
   unfold select.
   intros.
-  destruct T.
-  - apply H in H0. inversion H0. apply add_injective in H2. apply to_nat_injective in H2. apply H2.
-  - destruct i as [i xi]; destruct j as [j xj]. apply H in H0. inversion H0. apply add_injective in H2.
-  apply to_nat_injective in H2. rewrite H2. reflexivity.
+  - destruct x as [i0 xi]; destruct y as [j0 xj]. apply H in H0. inversion H0. apply add_injective in H3.
+  apply to_nat_injective in H3. rewrite H3. split; reflexivity.
 Qed.
 
 Proposition group_conserve_injectivity:
-  forall T m n (v : [[T;m*n]]), isInjective v -> isInjective (group m n v).
+  forall T (m : nat) (n : nat), conserveInjectivity (group m n) (A := [[T;m*n]]).
 Proof.
-  unfold isInjective. unfold group.
+  unfold conserveInjectivity. unfold group.
   intros.
-  destruct T; destruct i as [i1 j1]; destruct j as [i2 j2].
-  - apply H in H0. inversion H0. assert ((to_nat j1,to_nat i1) = (to_nat j2,to_nat i2)). apply projection_injective with (k := m). apply BoundedInt. apply BoundedInt. apply H2.
-  injection H1. intros. apply to_nat_injective in H3. rewrite H3.  apply to_nat_injective in H4. rewrite H4. reflexivity.
-  - destruct j1 as [j1 x1]; destruct j2 as [j2 x2]. apply H in H0. inversion H0. assert ((to_nat j1,to_nat i1) = (to_nat j2,to_nat i2)). apply projection_injective with (k := m). apply BoundedInt. apply BoundedInt. apply H2.
-  injection H1. intros. apply to_nat_injective in H4. rewrite H4.  apply to_nat_injective in H5. rewrite H5. reflexivity.
+  destruct x as [i1 j1]; destruct y as [i2 j2].
+  - destruct j1 as [j1 x1]; destruct j2 as [j2 x2]. apply H in H0. inversion H0. assert ((to_nat j1,to_nat i1) = (to_nat j2,to_nat i2)). apply projection_injective with (k := m). apply BoundedInt. apply BoundedInt. apply H3.
+  injection H1. intros. apply to_nat_injective in H5. rewrite H5.  apply to_nat_injective in H6. rewrite H6. split; reflexivity.
 Qed.
 
 Proposition transpose_conserve_injectivity :
-  forall T m n (v : [[[[T;m]];n]]), isInjective v -> isInjective (transpose v).
+  forall T (m : nat) (n : nat), conserveInjectivity transpose (A := [[[[T;m]];n]]).
 Proof.
-  unfold isInjective. unfold transpose.
+  unfold conserveInjectivity. unfold transpose.
   intros.
-  destruct T; destruct i as [i1 j1]; destruct j as [i2 j2].
-  - apply H in H0. injection H0. intros. subst. reflexivity.
-  - destruct j1 as [j1 x1]; destruct j2 as [j2 x2]. apply H in H0. injection H0. intros. subst. reflexivity.
+  destruct x as [i1 j1]; destruct y as [i2 j2].
+  - destruct j1 as [j1 x1]; destruct j2 as [j2 x2]. apply H in H0. injection H0. intros. subst.
+  split; reflexivity.
 Qed.
 
-Fixpoint non_empty_dimension (T : List nat) : Prop :=
+Proposition map_conserve_injectivity :
+  forall A B (n : nat) (f : ViewArray A -> ViewArray B),
+    conserveInjectivity f ->
+    conserveInjectivity (map f) (A := [[A;n]]).
+Proof.
+  intros. unfold conserveInjectivity in *. unfold map in *.
+    intros. destruct x,y. 
+    unfold isInjective in *. simpl in *.
+    
+Qed.
+
+
+
+(* Fixpoint non_empty_dimension (T : List nat) : Prop :=
   match T with
   | [] => True
   | h::tl => (h > 0) /\ (non_empty_dimension tl)
@@ -169,72 +191,23 @@ Lemma non_empty_tuple_inhabited :
 Proof.
   induction d.
   - intros. simpl. exists I. apply I.
-  - destruct d.
-    + simpl. intros. destruct H. exists (Id h 0 H). apply I.
-    + simpl. simpl in IHd. intros. destruct H. apply IHd in H0. destruct H0.
+  - simpl. simpl in IHd. intros. destruct H. apply IHd in H0. destruct H0.
       exists (Id h 0 H, x). apply I.
 Qed.
 
 
 Proposition app_conserve_injectivity :
-  forall T n (v : [[T;n]]), non_empty_dimension T -> isInjective v -> forall i j, (partialApp v i) = (partialApp v j) -> i = j
+  forall T n (v : ViewArray [[T;n]]), non_empty_dimension T -> isInjective v -> forall i j, (partialApp v i) = (partialApp v j) -> i = j
 .
 Proof.
   unfold isInjective.
   intros T n v Hd H i j H0.
-  destruct T.
-  - apply H in H0. apply H0.
-  - unfold partialApp in H0.
+    unfold partialApp in H0.
     apply non_empty_tuple_inhabited in Hd. destruct Hd. 
-    assert (forall x, (fun x : Tuple (h :: T) => v (i, x)) x = (fun x : Tuple (h :: T) => v (j, x)) x).
+    assert (forall x, (fun x : Tuple T => v (i, x)) x = (fun x : Tuple T => v (j, x)) x).
     { apply FunEquality. apply H0. }
-    assert ((fun x : Tuple (h :: T) => v (i, x)) x = (fun x : Tuple (h :: T) => v (j, x)) x).
+    assert ((fun x : Tuple T => v (i, x)) x = (fun x : Tuple T => v (j, x)) x).
     apply H2.
     simpl in H3. apply H in H3. injection H3. trivial.
 Qed.
-
-Definition funInjective {A : List nat} {B : List nat} : (ViewArray A -> ViewArray B) -> Prop :=
-  match B with
-  | [] => fun (f : ViewArray A -> ViewArray []) => forall v v', f v = f v' -> v = v'
-  | B => fun (f : ViewArray A -> ViewArray B) =>
-  forall n (v:[[A;n]]) a b x y, isInjective v -> f (partialApp v a) x = f (partialApp v b) y -> x = y /\ a = b
-end.
-
-  
-
-Proposition map_conserve_injectivity :
-  forall (A : List nat) (B : List nat) (f : ViewArray A -> ViewArray B) (n : nat) (v : [[A;n]]),
-    non_empty_dimension A ->
-    (forall v, isInjective v -> isInjective (f v)) -> funInjective f ->
-    isInjective v -> isInjective (map f v).
-Proof.
-  intros. unfold isInjective in *. unfold map in *. unfold funInjective in *.
-  destruct A.
-  - destruct B.
-    + simpl in *.
-    intros. apply H1 in H3. apply H2 in H3. apply H3.
-    + intros. destruct i; destruct j. simpl in H3. simpl in H1. apply H1 in H3. destruct H3. subst. reflexivity.
-    apply H2.
-  - destruct B. 
-    + intros. destruct i; destruct j. apply H1 in H3. simpl in H3.
-    assert (forall x, v (Id n n0 H4, x) = v (Id n n1 H5, x)). apply FunEquality.
-    apply H3. apply non_empty_tuple_inhabited in H. destruct H.
-    assert (v (Id n n0 H4, x) = v (Id n n1 H5, x)). apply H6.
-    apply H2 in H7. injection H7. intros. subst. apply unif_Idx.
-    + intros. destruct i; destruct j. simpl in H3. simpl in H1. apply H1 in H3.
-    destruct H3. subst. reflexivity.
-    apply H2.
-Qed.
-
-Proposition reverse_is_ok :
-  forall T n, funInjective reverse (A := n::T) (B := n::T).
-Proof.
-  intros. unfold funInjective. unfold partialApp. intros.
-  destruct T.
-  - apply H in H0. inversion H0.
-    apply sub_injective with (c := n - 1) in H3. apply to_nat_injective in H3. split. apply H3. reflexivity.
-    destruct n. destruct x. inversion H1. simpl. rewrite Nat.sub_0_r. destruct x. simpl. apply le_S_n. apply H1.
-    destruct n. destruct y. inversion H1. simpl. rewrite Nat.sub_0_r. destruct y. simpl. apply le_S_n. apply H1.
-  - 
-
-
+*)
