da273b47205789f77769279c1f272ccaeeda938d
Nicolas VINCENT <nicolas.vincent1@ens-rennes.fr>
2025-06-24 13:50:14 +0200

    Update Examples.v

--- Examples.v
+++ Examples.v
@@ -2,6 +2,7 @@
 From Views Require Import BoundedInt.
 From Views Require Import ViewFunctions.
 From Views Require Import ViewsProof.
+From Views Require Import ViewsLemmas.
 Require Import PeanoNat.
 
 Axiom FunEquality :
@@ -24,20 +25,20 @@ Proof. auto. Qed.
 Example testmap :
   (map reverse (group 3 (identity_view (3*2)))) = 
   (fun x y => match (x,y) with 
-            | (idx _ 0 _,idx _ 0 _) => idx 6 2 H2'
-            | (idx _ 0 _,idx _ 1 _) => idx 6 1 H1'
-            | (idx _ 0 _,idx _ 2 _) => idx 6 0 H0'
-            | (idx _ 1 _,idx _ 0 _) => idx 6 5 H5'
-            | (idx _ 1 _,idx _ 1 _) => idx 6 4 H4'
-            | (idx _ 1 _,idx _ 2 _) => idx 6 3 H3'
-            | _ => idx 6 0 H0'
+            | (idx _ 0 _,idx _ 0 _) => 2
+            | (idx _ 0 _,idx _ 1 _) => 1
+            | (idx _ 0 _,idx _ 2 _) => 0
+            | (idx _ 1 _,idx _ 0 _) => 5
+            | (idx _ 1 _,idx _ 1 _) => 4
+            | (idx _ 1 _,idx _ 2 _) => 3
+            | _ => 0
 end).
 Proof.
   simpl. unfold map. unfold identity_view. unfold group. unfold reverse. 
   apply FunEquality. intros. apply FunEquality. intro y. destruct x as [x Hx]. destruct y as [y Hy]. destruct x.
-  - simpl. destruct y. apply unif_Idx. destruct y. apply unif_Idx. destruct y. apply unif_Idx. exfalso. inversion Hy. inversion H0. inversion H2. inversion H4.
+  - simpl. destruct y. reflexivity. destruct y. reflexivity. destruct y. reflexivity. exfalso. inversion Hy. inversion H0. inversion H2. inversion H4.
   - destruct x.
-    + simpl. destruct y. apply unif_Idx. destruct y. apply unif_Idx. destruct y. apply unif_Idx. exfalso. inversion Hy. inversion H0. inversion H2. inversion H4.
+    + simpl. destruct y. reflexivity. destruct y. reflexivity. destruct y. reflexivity. exfalso. inversion Hy. inversion H0. inversion H2. inversion H4.
     + exfalso. inversion Hx. inversion H0. inversion H2.
 Qed.
 
@@ -49,11 +50,11 @@ Proof.
   apply BoundedInt. apply H.
 Qed.
 
-Definition index_identity {l : nat} {T : List nat} {n : nat} (H : l >= n) (v : ViewArray l [[T;n]]) : ViewArray l [[;n]] :=
-  fun i => idx l (to_nat i) (inf l n i H).
+Definition index_identity {T : List nat} {n : nat} (v : ViewArray [[T;n]]) : ViewArray [[;n]] :=
+  fun i => to_nat i.
 
 Example index_identity_does_not_preserve_injectivity :
-  (forall l T n (H : l >= n), preserve_Injectivity (index_identity H) (l := l) (A := (n::T))) -> False.
+  (forall T n, preserve_Injectivity index_identity (A := (n::T))) -> False.
 Proof.
   intros.
   unfold preserve_Injectivity in H.
@@ -64,8 +65,7 @@ Proof.
     apply preserve_Injectivity_implies_preserving_view_injectivity. apply group_preserves_injectivity.
     apply H2. apply identity_view_injective.
   assert (((idx 2 1 H1,I), (idx 2 0 H0,I)) = ((idx 2 0 H0,I), (idx 2 0 H0,I))).
-  assert ((2*2) >= 2). simpl. auto.
-  apply H with (v := (group 2 (identity_view (2*2)))) (H := H3) (C := (2::[]))
+  apply H with (v := (group 2 (identity_view (2*2)))) (C := (2::[]))
       (i := (idx 2 1 H1,I))
       (x := (idx 2 0 H0,I))
       (j := (idx 2 0 H0,I))
