0cce82d3620c987cda1b6538e5d8bc115ba93ac7
Nicolas VINCENT <nicolas.vincent1@ens-rennes.fr>
2025-06-25 15:09:53 +0200

    commenting the proof for swap

--- Examples_automation.v
+++ Examples_automation.v
@@ -8,7 +8,7 @@ From Views Require Import Tactic.
 Require Import PeanoNat.
 
 (** Proofs that are in `ViewsProof.v` using the automated tactics from `Tactic.v`.
-Does not contain the proof of `map` preserving injectivity as it is not easily automatable 
+Does not contain the proof of `map` preserving injectivity as it is not easily automatable by
 following a similar pattern as the others *)
 
 Theorem reverse_preserves_injectivity_auto_proof : forall T n, preserve_Injectivity reverse (A := (n::T)).
@@ -16,6 +16,7 @@ Proof.
   intros T n.
   set (function := fun (x : Tuple (n::T)) => match x with | (i,tx) => (idx n (n - 1 - to_nat i) reverseProof,tx) end).
   assert (H':forall (x y : Idx n), n - 1 - to_nat x = n - 1 - to_nat y -> x = y). {
+    (* assertion : the reordering function used by reverse is injective *)
     intros.
     apply sub_injective in H.
     apply to_nat_injective in H. subst. reflexivity.
@@ -67,6 +68,7 @@ Proof.
   intros T n m.
   set (function := fun (x : Tuple (n::m::T)) => match x with | (i,(j,tx)) => (idx (m*n) (to_nat j + m*(to_nat i)) groupProof,tx) end).
   assert (H':forall (x1 y1 : Idx m) (x2 y2 : Idx n), to_nat x1 + m * to_nat x2 = to_nat y1 + m * to_nat y2 -> (x1,x2) = (y1,y2)). {
+    (* assertion : the reordering function used by reverse is injective *)
     intros.
     apply projection_injective in H. inversion H. apply to_nat_injective in H1. apply to_nat_injective in H2. subst. reflexivity.
     apply BoundedInt. apply BoundedInt.
@@ -112,11 +114,13 @@ Proof.
   intros T n i j.
   set (function := fun (x : Tuple (n::T)) => match x with | (i',tx) => (if (eqb (to_nat i') (to_nat i)) then j else if (eqb (to_nat i') (to_nat j)) then i else i',tx) end).
   reordering_autoProof1 0;
+  (* destruct equalities *)
   destruct (eqb (to_nat i0) (to_nat i)) eqn:Ex;
   destruct (eqb (to_nat i0) (to_nat j)) eqn:Ex';
   destruct (eqb (to_nat i1) (to_nat i)) eqn:Ey;
   destruct (eqb (to_nat i1) (to_nat j)) eqn:Ey';
   unfold swap in *; simpl in Hypothesis1;
+  (* replace eqb by actual equalities *)
   try (rewrite Ex in Hypothesis1);
   try (apply eqb_impl_eq in Ex; apply to_nat_injective in Ex);
   try (rewrite Ex' in Hypothesis1);
@@ -126,6 +130,7 @@ Proof.
   try (rewrite Ey' in Hypothesis1);
   try (apply eqb_impl_eq in Ey'; apply to_nat_injective in Ey');
   applyHinj_all(); try reflexivity. (* solve 24 of the 32 cases *)
+  (* manually handling the absurd cases *)
   rewrite eqb_n_n in Ey';inversion Ey'.
   rewrite eqb_n_n in Ey;inversion Ey.
   rewrite eqb_n_n in Ex';inversion Ex'.
